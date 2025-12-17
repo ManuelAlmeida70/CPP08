fcc7277a7f3f2e098805daba9cacebff214e536a
Manuel Mazunga De Almeida <maalmeid@c2r10s2.42luanda.com>
2025-12-17 15:33:12 +0100

    doing main ex00

---
+++ ex00/easyfind.hpp
@@ -0,0 +1,30 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <exception>
+#include <algorithm>
+#include <iostream>
+
+class ValueNotFoundException : std::exception
+{
+    public:
+        virtual const char *what() const throw()
+        {
+            return "Valor nao encontrado";
+        }
+};
+
+template <typename T>
+typename T::iterator easyfind(T& container, int toFind)
+{
+    typename T::iterator it = std::find(container.begin(), container.end(), toFind);
+
+    if (it == container.end())
+    {
+        throw ValueNotFoundException();
+    }
+
+    return it;
+}
+
+#endif
